b0be789e62a2930cc2e8a8df9d50135601488e90
Lukas <orpiczymejl@gmail.com>
2023-01-14 23:54:48 +0100

    error handling, when edge was deleted during rerouting procedure

--- lib/nav.py
+++ lib/nav.py
@@ -13,7 +13,7 @@ office_graph_dict = {0: [1, 3],
                      7: [4, 6, 8],
                      8: [5, 7]}
 
-blocked_edges = [(0,1),(1,2)]
+blocked_edges = [(0, 1), (1, 2), (3, 4)]
 blocked_nodes = [9]
 
 office_graph = nx.Graph(office_graph_dict)
@@ -51,7 +51,7 @@ class Navigator:
 
         self.reset_visited()
         self.print_status()
-        print("PATH >>> ",self.main_path)
+        print("PATH >>> ", self.main_path)
         while not self.check_if_all_edges_and_nodes_are_visited():
 
             is_obstacle_in_our_way = self.detect_obstacle(self.next_node)
@@ -60,12 +60,11 @@ class Navigator:
 
                 self.print_status()
                 self.update_visited(self.current_node, self.next_node)
-                
+
                 self.send_cmd(self.next_node)
                 self.current_node = self.next_node
-                
-                
-                print("Popping  from main path : ",self.main_path.pop(0))
+
+                print("Popping  from main path : ", self.main_path.pop(0))
                 if not self.check_if_all_edges_and_nodes_are_visited():
                     next_edge = self.main_path[0]
                     self.next_node = next_edge[1]
@@ -74,16 +73,15 @@ class Navigator:
 
                 self.remove_edge((self.current_node, self.next_node))
                 self.go_around()
-
-        self.print_summary()
         
+        self.prepare_results()
+        self.print_summary()
 
     def go_around(self):
 
         print("Obstacle >>> going around")
         while self.current_node != self.next_node:
 
-            
             try:
                 auxilary_path = nx.shortest_path(
                     self.map, self.current_node, self.next_node)
@@ -101,9 +99,9 @@ class Navigator:
             is_obstacle_in_our_way = self.detect_obstacle(auxilary_path[1])
 
             if not is_obstacle_in_our_way:
-                
-                auxilary_path.pop(0) #gets rid of current node from path
-                
+
+                auxilary_path.pop(0)  # gets rid of current node from path
+
                 dest_node = auxilary_path[0]
                 self.send_cmd(dest_node)
                 self.current_node = dest_node
@@ -111,31 +109,36 @@ class Navigator:
             else:
                 self.remove_edge((self.current_node, auxilary_path[1]))
 
-            
             self.print_status()
-            
-        
-        self.update_visited(self.current_node,self.next_node)
-        
-        print("Popping  from main path : ",self.main_path.pop(0))
+
+        self.update_visited(self.current_node, self.next_node)
+
+        print("Popping  from main path : ", self.main_path.pop(0))
         next_edge = self.main_path[0]
         self.next_node = next_edge[1]
+        print("Finished rerouting")
 
-    def detect_obstacle(self,next_node):
+    def detect_obstacle(self, next_node):
         # return random.uniform(0, 1) > 1.1
-        return (self.current_node,next_node) in blocked_edges
+        return (self.current_node, next_node) in blocked_edges
 
     def remove_node(self, node_to_remove):
         self.graph.remove_node(node_to_remove)
         self.map = nx.eulerize(self.graph)
 
     def remove_edge(self, blocked_edge):
+        print("Removing edge ", blocked_edge)
         self.blocked_edges.append(blocked_edge)
-        self.graph.remove_edge(blocked_edge[0], blocked_edge[1])
+
+        try:
+            self.graph.remove_edge(blocked_edge[0], blocked_edge[1])
+        except:
+            print("INFO >>> removing not existing edge, it was propably removed during rerouting procedure = ", blocked_edge)
+
         self.map = nx.eulerize(self.graph)
-        
-        self.visited_edges.pop((blocked_edge[0], blocked_edge[1]),None)
-        self.visited_edges.pop((blocked_edge[1], blocked_edge[0]),None)
+
+        self.visited_edges.pop((blocked_edge[0], blocked_edge[1]), None)
+        self.visited_edges.pop((blocked_edge[1], blocked_edge[0]), None)
 
     def update_visited(self, start_node, end_node):
         self.visited_edges[(start_node, end_node)] = True
@@ -149,30 +152,42 @@ class Navigator:
     def check_if_all_edges_and_nodes_are_visited(self):
         return all(self.visited_nodes.values()) and all(self.visited_edges.values())
 
-    def send_cmd(self,destination):
+    def send_cmd(self, destination):
         print("<<< CMD >>>")
-        print("destination : ", destination )
-        
-    def print_status(self, verbose=False, debug_run = False):
+        print("destination : ", destination)
+
+    def print_status(self, verbose=False, debug_run=False):
         if not debug_run:
             return
-        
+
         print("<<SYSTEM STATUS>>")
         print("Current node : ", self.current_node,
               ", Next node : ", self.next_node)
 
         if verbose:
-            print("Visited nodes : ", [node for node, status in self.visited_nodes.items() if status])
-            print("NOT Visited nodes : ", [node for node, status in self.visited_nodes.items() if not status])
-            print("Visited edges : ", [edge for edge, status in self.visited_edges.items() if status])
-            print("NOT Visited edges : ", [edge for edge, status in self.visited_edges.items() if not status])
+            print("Visited nodes : ", [
+                  node for node, status in self.visited_nodes.items() if status])
+            print("NOT Visited nodes : ", [
+                  node for node, status in self.visited_nodes.items() if not status])
+            print("Visited edges : ", [
+                  edge for edge, status in self.visited_edges.items() if status])
+            print("NOT Visited edges : ", [
+                  edge for edge, status in self.visited_edges.items() if not status])
 
+    def prepare_results(self):
+        self.blocked_edges = set(self.blocked_edges)
+        self.isolated_nodes = set(self.isolated_nodes)
+        
     def print_summary(self):
         print("\n\n<<< FINAL RESULTS >>>")
         print("< OBSTACLES >")
         print("blocked edges: ", self.blocked_edges)
         print("isolated nodes: ", self.isolated_nodes)
-        
+
 
 nav = Navigator(office_graph)
+# try:
 nav.search()
+# except Exception as e:
+#     print("Error: ", e)
+#     nav.print_status(verbose=True,debug_run=True)
